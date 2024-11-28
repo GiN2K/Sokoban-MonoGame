5194acc9379c270d970209f7eb2fe508cca7a3c3
amayas_gh <amayas.Gherbi@etu.univ-grenoble-alpes.fr>
2024-11-28 15:48:05 +0100

    update assembly info and cache with new version identifiers

--- Sokoban/obj/Debug/net8.0/Sokoban.AssemblyInfo.cs
+++ Sokoban/obj/Debug/net8.0/Sokoban.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sokoban")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aaa896278a89c484be002e6c2fce1f513defe38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cda00a7aee49e85b2f6008c1e2f1cc1e5473adf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sokoban")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sokoban")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
